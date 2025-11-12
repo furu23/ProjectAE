26d400861f8f815b94888b591dae81fef7c26e93
jhk63 <163096026+jhk63@users.noreply.github.com>
2025-11-12 12:35:05 +0900

    폴더 오타 수정 & 위젯 생성

--- Source/ProjectAE/Private/Inventory/InventoryComponent.cpp
+++ Source/ProjectAE/Private/Inventory/InventoryComponent.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Inventroy/InventoryComponent.h"
+#include "Inventory/InventoryComponent.h"
 
 #include "Data/ItemData.h"
-#include "Inventroy/Data/InventorySlot.h"
+#include "Inventory/Data/InventorySlot.h"
 
 
 // Sets default values for this component's properties
