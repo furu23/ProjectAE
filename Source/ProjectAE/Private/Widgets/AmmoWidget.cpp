f0271cdf22a47961540fdf495bf049dc83ef44a4
박동진 <flelo456@gmail.com>
2025-12-18 14:53:18 +0900

    아모 위젯 정의 및 기본 구현 완료
    AmmoBaseClass는 다양한 이벤트를 제공하며, NVI 패턴을 통해 로직의 무결성을 보장합니다.
    
    구체 클래스는 실행 부분만 담당하고 있으며. 꾸미기 및 기타 ui 변경은 bp에서 작업할 수 있습니다.

---
+++ Source/ProjectAE/Private/Widgets/AmmoWidget.cpp
@@ -0,0 +1,147 @@
+#include "Widgets/AmmoWidget.h"
+#include "AbilitySystemComponent.h"
+#include "Characters/Player/PlayerCharacter.h"
+#include "ProjectAE/ProjectAE.h"
+#include "Characters/Player/AEWeaponComponent.h"
+#include "Components/TextBlock.h"
+#include "Components/ProgressBar.h"
+#include "Animation/WidgetAnimation.h"
+#include "Components/Image.h"
+
+void UAmmoWidgetBase::InitializeWidget(UAEWeaponComponent* WeaponComp, UAbilitySystemComponent* ASC)
+{
+    CachedWeaponComponent = WeaponComp;
+    CachedASC = ASC;
+
+    if (!ASC && !WeaponComp)
+    {
+        UE_LOG(LogAbilitySys, Error, TEXT("[AmmoWidget] Initailize failed for Invalid ASC argument."));
+        return;
+    }
+
+    ASC->RegisterGameplayTagEvent(FGameplayTag::RequestGameplayTag("State.Action.Reloading"), EGameplayTagEventType::NewOrRemoved)
+        .AddUObject(this, &UAmmoWidgetBase::OnReloadStateChanged);
+
+    WeaponComp->OnAmmoUseDelegate.AddDynamic(this, &UAmmoWidgetBase::UpdateAmmoState);
+
+    UpdateAmmoState(WeaponComp->GetCurrentAmmo(), CurrentAmmo);
+    MaxAmmo = WeaponComp->GetMaxAmmo();
+
+    NativeOnBind();
+    K2_OnInitialized(WeaponComp, ASC);
+}
+
+void UAmmoWidgetBase::UpdateAmmoState(int32 NewValue, int32 OldValue)
+{
+    CurrentAmmo = NewValue;
+
+    NativeOnAmmoUpdated(NewValue, OldValue);
+    K2_OnAmmoAmountChanged(NewValue, OldValue);
+}
+
+void UAmmoWidgetBase::OnReloadStateChanged(FGameplayTag ReloadTag, int32 Level)
+{
+    if (!CachedASC.IsValid())
+    {
+        UE_LOG(LogAbilitySys, Error, TEXT("[AmmoWidget] Initailize failed for Invalid ASC argument."));
+        return;
+    }
+    if (!CachedWeaponComponent.IsValid() && !TryRefreshAmmoComp())
+    {
+        return;
+    }
+
+    if (Level == 0)
+    {
+        OnReloadEnded();
+        K2_OnReloadStateEnded(CachedWeaponComponent.Get(), CachedASC.Get());
+    }
+    else
+    {
+        OnReloadStarted();
+        K2_OnReloadStateStarted(CachedWeaponComponent.Get(), CachedASC.Get());
+    }
+}
+
+UAEWeaponComponent * UAmmoWidgetBase::TryRefreshAmmoComp()
+{
+    if (CachedWeaponComponent.IsValid())
+    {
+        return CachedWeaponComponent.Get();
+    }
+
+    UAbilitySystemComponent* ASC = CachedASC.Get();
+    if (!ASC) return nullptr;
+
+    APlayerCharacter* PlayerChar = Cast<APlayerCharacter>(ASC->GetAvatarActor());
+    if (!PlayerChar) return nullptr;
+
+    UAEWeaponComponent* FoundComp = PlayerChar->GetWeaponComponent();
+
+    if (FoundComp)
+    {
+        CachedWeaponComponent = FoundComp;
+
+        UE_LOG(LogAbilitySys, Warning, TEXT("[AmmoWidget] WeaponComp was missing but recovered manually. Actor: %s. Check InitializeWidget timing!"),
+            *GetOwningPlayerPawn()->GetName());
+
+        return FoundComp;
+    }
+
+    UE_LOG(LogAbilitySys, Error, TEXT("[AmmoWidget] WeaponComp was missing and Failed to find manually!"));
+
+    return nullptr;
+}
+
+void UAmmoWidget::NativeOnBind()
+{
+    if (Text_MaxAmmo)
+    {
+        Text_MaxAmmo->SetText(FText::AsNumber(MaxAmmo));
+    }
+}
+
+void UAmmoWidget::NativeOnAmmoUpdated(int32 NewValue, int32 OldValue)
+{
+    UpdateVisuals();
+}
+
+void UAmmoWidget::OnReloadStarted()
+{
+    if (Image_Reload)
+    {
+        Image_Reload->SetVisibility(ESlateVisibility::HitTestInvisible);
+    }
+
+    if (Anim_Reload)
+    {
+        PlayAnimation(Anim_Reload, 0.0f, 0);
+    }
+}
+
+void UAmmoWidget::OnReloadEnded()
+{
+    if (Anim_Reload)
+    {
+        StopAnimation(Anim_Reload);
+    }
+
+    if (Image_Reload)
+    {
+        Image_Reload->SetVisibility(ESlateVisibility::Hidden);
+    }
+}
+
+void UAmmoWidget::UpdateVisuals()
+{
+    if (Text_CurrentAmmo)
+    {
+        Text_CurrentAmmo->SetText(FText::AsNumber(CurrentAmmo));
+    }
+
+    if (Bar_AmmoGauge && MaxAmmo > 0)
+    {
+        const float Percent = (float)CurrentAmmo / (float)MaxAmmo;
+        Bar_AmmoGauge->SetPercent(Percent);
+    }
+}
---
+++ Source/ProjectAE/Private/Widgets/AmmoWidget.h
@@ -0,0 +1,104 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameplayTagContainer.h"
+#include "AmmoWidget.generated.h"
+
+class UAbilitySystemComponent;
+class UAEWeaponComponent;
+class UTextBlock;
+class UProgressBar;
+class UImage;
+class UWidgetAnimation;
+
+UCLASS(Abstract)
+class UAmmoWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void InitializeWidget(UAEWeaponComponent* WeaponComp, UAbilitySystemComponent* ASC);
+
+protected:
+	// 콜백 및 내부 non virtual 함수
+	UFUNCTION()
+	void UpdateAmmoState(int32 NewValue, int32 OldValue);
+
+	void OnReloadStateChanged(FGameplayTag ReloadTag, int32 Level);
+
+
+	// NVI API 순수 가상 함수
+	virtual void NativeOnAmmoUpdated(int32 NewValue, int32 OldValue) {}
+
+	virtual void NativeOnBind() {}
+
+	virtual void OnReloadStarted() {};
+
+	virtual void OnReloadEnded() {};
+
+
+	// BP 훅 API 함수들
+	UFUNCTION(BlueprintImplementableEvent, Category = "UI", meta = (DisplayName = "OnInitialized"))
+	void K2_OnInitialized(UAEWeaponComponent* WeaponComp, UAbilitySystemComponent* ASC);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "UI", meta = (DisplayName = "OnReloadStateStarted"))
+	void K2_OnReloadStateStarted(UAEWeaponComponent* WeaponComp, UAbilitySystemComponent* ASC);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "UI", meta = (DisplayName = "OnReloadStateEnded"))
+	void K2_OnReloadStateEnded(UAEWeaponComponent* WeaponComp, UAbilitySystemComponent* ASC);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "UI", meta = (DisplayName = "OnAmmoAmountChanged"))
+	void K2_OnAmmoAmountChanged(int32 NewValue, int32 OldValue);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Ammo")
+	int32 CurrentAmmo = -1;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Ammo")
+	int32 MaxAmmo = -1;
+
+private:
+	UAEWeaponComponent* TryRefreshAmmoComp();
+
+	TWeakObjectPtr<UAEWeaponComponent> CachedWeaponComponent;
+
+	TWeakObjectPtr<UAbilitySystemComponent> CachedASC;
+};
+
+UCLASS()
+class UAmmoWidget : public UAmmoWidgetBase
+{
+	GENERATED_BODY()
+
+
+protected:
+	virtual void NativeOnBind() override;
+
+	virtual void NativeOnAmmoUpdated(int32 NewValue, int32 OldValue) override;
+
+	virtual void OnReloadStarted() override;
+
+	virtual void OnReloadEnded() override;
+
+	void UpdateVisuals();
+
+	UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UTextBlock> Text_CurrentAmmo;
+
+    UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UTextBlock> Text_MaxAmmo;
+
+    UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UProgressBar> Bar_AmmoGauge;
+
+    UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UImage> Image_WeaponIcon;
+
+	UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UImage> Image_Reload;
+
+	UPROPERTY(meta = (BindWidgetAnimOptional), Transient)
+	TObjectPtr<UWidgetAnimation> Anim_Reload;
+};
